ff50c721a8ed0b313404567f32c82cf9736963c6
Xianyi Cheng <xianyic@andrew.cmu.edu>
2020-01-16 12:19:02 -0500

    sliding sticking exponential enumeration

--- python/src/contact_modes/modes_3d.py
+++ python/src/contact_modes/modes_3d.py
@@ -1,10 +1,10 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import scipy as sp
 import pyhull
 from pyhull.halfspace import Halfspace
-from .helpers import hat, lexographic_combinations
+from .helpers import hat, lexographic_combinations, exp_comb, zenotope_vertex
 from .interior_point import interior_point_halfspace
-
 
 DEBUG = False
 
@@ -19,7 +19,7 @@ def contacts_to_half(points, normals):
     A *= -1
     b = np.zeros((n_pts, 1))
     return A, b
-    
+
 
 def enumerate_contact_separating_3d_exponential(points, normals):
     # Check inputs dimensions.
@@ -71,7 +71,7 @@ def enumerate_contact_separating_3d_exponential(points, normals):
                 if DEBUG:
                     print('projecting H into null space')
                     print(H)
-            
+
             # Compute interior point.
             b = np.zeros((H.shape[0], 1))
             x = interior_point_halfspace(H, b)
@@ -86,6 +86,7 @@ def enumerate_contact_separating_3d_exponential(points, normals):
 
     return np.array(sorted(modes))
     # return np.array(modes)
+
 
 def enumerate_contact_separating_3d(points, normals):
     # Check inputs dimensions.
@@ -150,3 +151,160 @@ def enumerate_contact_separating_3d(points, normals):
 
 def enum_sliding_sticking_3d(points, normals, cs_mode):
     pass
+
+
+def enumerate_all_modes_3d_exponential(points, normals, tangentials, num_sliding_modes):
+    # Check inputs dimensions.
+    assert (points.shape[1] == normals.shape[1])
+    assert (points.shape[0] == 3)
+    assert (normals.shape[0] == 3)
+    assert (tangentials.shape[0] == 3)
+    assert (tangentials.shape[2] == 2) #  two tangentials vectors: x and y
+
+    # Get dimensions.2
+    n_pts = points.shape[1]
+
+    # Create halfspace inequalities, Ax - b ≥ 0.
+    n_pts = points.shape[1]
+    A = np.zeros((n_pts, 6))
+    for i in range(n_pts):
+        A[i, 0:3] = normals[:, i].flatten()
+        A[i, 3:6] = np.dot(normals[:, i].T, hat(points[:, i])).flatten()
+    # A *= -1
+    b = np.zeros((n_pts, 1))
+    if DEBUG:
+        print('A')
+        print(A)
+
+    # Get linearized sliding sections from number of sliding modes
+    assert (num_sliding_modes % 2 == 0)
+    D = np.array([[np.sin(2*np.pi*i/num_sliding_modes),np.cos(2*np.pi*i/num_sliding_modes),0] for i in range(num_sliding_modes)])
+    # Get sliding mode section enumeration
+    sliding_sections = np.identity(num_sliding_modes) - np.roll(np.identity(num_sliding_modes),1,axis=1)
+    T = np.zeros((n_pts,num_sliding_modes,2,6))
+    T_fixed = np.zeros((n_pts,4,6))
+    for i in range(n_pts):
+        D_i = np.concatenate((tangentials[:,i,:].T,-tangentials[:,i,:].T))
+        T_i = [np.dot(normals[:, i].T, hat(t)) for t in D_i]
+        T_fixed[i, :, 0:3] = T_i
+        T_fixed[i, :, 3:6] = np.dot(T_i, hat(points[:, i]))
+        for j in range(num_sliding_modes):
+            section = sliding_sections[j]
+            D_i = (section*D.T).T
+            D_i = D_i[section!=0]
+            T_i = [np.dot(normals[:,i].T,hat(t)) for t in D_i]
+            T[i,j,:,0:3] = T_i
+            T[i, j, :, 3:6] = np.dot(T_i, hat(points[:, i]))
+
+    # Enumerate contact modes and check for feasibility.
+    modes = []
+    for i in range(0, n_pts + 1):
+        for c in lexographic_combinations(n_pts, i):
+            if DEBUG:
+                print('c', c)
+            mask = np.zeros(n_pts, dtype=bool)
+            mask[c] = 1
+            if DEBUG:
+                print('mask', mask)
+            C = A[mask, :]
+            H = A[~mask, :]
+
+            # Skip case where all contacts active.
+
+            if np.sum(mask) == n_pts:
+                check_sliding = True
+            else:
+
+                # Project into null space.
+                if np.sum(mask) > 0:
+                    null = sp.linalg.null_space(C)
+                    H = np.dot(H, null)
+                    if DEBUG:
+                        print('projecting H into null space')
+                        print(H)
+
+                # Compute interior point.
+                b = np.zeros((H.shape[0], 1))
+                x = interior_point_halfspace(H, b)
+                check_sliding = 1e-5 < np.linalg.norm(np.dot(H, x))
+
+            # If point is strictly interior, then the mode string is valid.
+            if check_sliding:
+                m = np.array(['s'] * n_pts)
+                c_normals = normals[:,c]
+                c_points = points[:,c]
+                c = np.array(c)
+                # check sliding modes
+                # sliding mode enumeration
+                sliding_modes = exp_comb(len(c), num_sliding_modes+1) # enumeration sliding sections
+                if len(c)==0:
+                    modes.append(m.tolist())
+                    continue
+                for s_mode in sliding_modes:
+                    # get sliding matrix T
+
+                    c_sliding = c[s_mode < num_sliding_modes]
+                    mode_sliding = s_mode[s_mode < num_sliding_modes]
+                    c_fixed= c[s_mode == num_sliding_modes]
+                    Ts = np.concatenate((T[c_sliding,mode_sliding,:,:].reshape(-1,6),T_fixed[c_fixed,:,:].reshape(-1,6)))
+                    # check interior-
+                    if len(H) == 0:
+                        M = Ts
+                    else:
+                        Ts = np.dot(Ts, null)
+                        M = np.concatenate((H,Ts))
+                    b = np.zeros((M.shape[0], 1))
+                    x = interior_point_halfspace(M, b)
+                    if 1e-5 < np.linalg.norm(np.dot(M, x)):
+                        s_mode_str = [str(s_mode[i]) for i in range(len(s_mode))]
+                        m[c] = s_mode_str
+                        modes.append(m.tolist())
+                        if DEBUG:
+                            print('Appending mode', m.tolist())
+
+    return np.array(sorted(modes))
+
+
+def enumerate_all_modes_3d(points, normals, tangentials, num_sliding_modes):
+    # Check inputs dimensions.
+    assert (points.shape[1] == normals.shape[1])
+    assert (points.shape[0] == 3)
+    assert (normals.shape[0] == 3)
+    assert (tangentials.shape[0] == 3)
+    assert (tangentials.shape[2] == 2) #  two tangentials vectors: x and y
+
+    # Get dimensions.2
+    n_pts = points.shape[1]
+
+    # Create halfspace inequalities, Ax - b ≥ 0.
+    n_pts = points.shape[1]
+    A = np.zeros((n_pts, 6))
+    for i in range(n_pts):
+        A[i, 0:3] = normals[:, i].flatten()
+        A[i, 3:6] = np.dot(normals[:, i].T, hat(points[:, i])).flatten()
+
+    # Get linearized sliding sections from number of sliding modes
+    assert (num_sliding_modes % 2 == 0)
+    D = np.array([[np.sin(2*np.pi*i/num_sliding_modes),np.cos(2*np.pi*i/num_sliding_modes),0] for i in range(num_sliding_modes)])
+    T = np.zeros((n_pts,num_sliding_modes,6)) # sliding plane normals
+    for i in range(n_pts):
+        R = np.concatenate((normals[:, i].reshape(-1,1), tangentials[:, i, :]), axis=1)
+        for j in range(num_sliding_modes):
+            T_i = np.dot(np.linalg.inv(R),D[j])
+            T[i,j,0:3] = T_i
+            T[i,j,3:6] = np.dot(T_i, hat(points[:, i]))
+
+    # keep track for the modes of each face normal
+    s_mode_str = [str(i) for i in range(num_sliding_modes)]
+    N_modes = np.hstack((np.array(['s']*n_pts),np.matlib.repmat(s_mode_str,1,n_pts).flatten()))
+    N = np.vstack((A,T.reshape(-1,T.shape[2])))
+
+    V, Sign = zenotope_vertex(N)# TODO: debug this!!!
+
+    vertices = [list(V[i]) for i in range(V.shape[0])]
+    ret = pyhull.qconvex('s Fv', vertices)
+    print(np.array(ret))
+
+    # get the convex hull of V
+    # select faces with desired modes
+    return ret
--- python/tests/test_modes_3d.py
+++ python/tests/test_modes_3d.py
@@ -5,7 +5,7 @@ import numpy as np
 import contact_modes
 from contact_modes import (FaceLattice, enumerate_contact_separating_3d,
                            enumerate_contact_separating_3d_exponential)
-
+from contact_modes.helpers import in_hull
 
 def test_enum_contact_separate_3d():
     # Create four planar contact points.
@@ -18,11 +18,12 @@ def test_enum_contact_separate_3d():
     normals[2,:] = 1.0
     # print(points)
     # print(normals)
-    # contact_modes.enumerate_contact_separating_3d(points, normals)
-    modes = enumerate_contact_separating_3d_exponential(points, normals)
+    #
+    modes = contact_modes.enumerate_contact_separating_3d_exponential(points, normals)
+    print('contact mode: ')
     print(modes)
     # modes = enumerate_contact_separating_3d(points, normals)
-
+    '''
     # Create contact manifold in the shape of an octagon.
     n = 6
     points = np.zeros((3,n))
@@ -31,7 +32,7 @@ def test_enum_contact_separate_3d():
         points[0,i] = np.cos(i/8*2*np.pi)
         points[1,i] = np.sin(i/8*2*np.pi)
         normals[2,i] = 1.0
-    modes = enumerate_contact_separating_3d_exponential(points, normals)
+    modes = contact_modes.enumerate_contact_separating_3d(points, normals)
     print(modes)
     # modes = enumerate_contact_separating_3d(points, normals)
 
@@ -53,10 +54,10 @@ def test_enum_contact_separate_3d():
     normals[1,4:8] =-1.0
 
     t_start = time()
-    modes = enumerate_contact_separating_3d_exponential(points, normals)
+    modes = contact_modes.enumerate_contact_separating_3d(points, normals)
     print('time', time() - t_start)
     print(modes)
-    print(modes.shape)
+    #print(modes.shape)
     t_start = time()
     # modes = enumerate_contact_separating_3d(points, normals)
     enumerate_contact_separating_3d(points, normals)
@@ -72,11 +73,11 @@ def test_enum_contact_separate_3d():
                   [0, 1, 1, 0, 1, 1],
                   [0, 0, 1, 1, 1, 1]])
     d = 4
-    
+
     t_start = time()
     L = FaceLattice(M, d)
     print('time', time() - t_start)
-    
+
     print(L.num_proper_faces())
     print(L.num_faces())
     # print(L.mode_strings())
@@ -104,8 +105,80 @@ def test_enum_contact_separate_3d():
     points[:,7] = np.array([ 1,-1, 1])
     normals[2,0:4] = 1.0
     normals[2,4:8] =-1.0
-    modes = enumerate_contact_separating_3d_exponential(points, normals)
+    modes = contact_modes.enumerate_contact_separating_3d(points, normals)
     print(modes)
     # modes = enumerate_contact_separating_3d(points, normals)
+    #assert(False)
+    '''
 
-    assert(False)
+def test_enum_contact_all_3d():
+
+    # Create four planar contact points.
+    points = np.zeros((3, 4))
+    normals = np.zeros((3, 4))
+    points[:, 0] = np.array([1, 1, 0])
+    points[:, 1] = np.array([-1, 1, 0])
+    points[:, 2] = np.array([-1, -1, 0])
+    points[:, 3] = np.array([1, -1, 0])
+    normals[2, :] = 1.0
+    # print(points)
+    # print(normals)
+    #
+    tangentials = np.zeros((3,4,2))
+    tangentials[0,:,0] = 1
+    tangentials[1,:,1] = 1
+    #modes = contact_modes.enumerate_all_modes_3d_exponential(points, normals,tangentials,4)
+    modes = contact_modes.enumerate_all_modes_3d(points, normals,tangentials,4)
+    print('contact mode: ')
+    print(modes)
+    '''
+    # Create contact manifold in the shape of an octagon.
+    n = 6
+    points = np.zeros((3,n))
+    normals = np.zeros((3,n))
+    for i in range(n):
+        points[0,i] = np.cos(i/8*2*np.pi)
+        points[1,i] = np.sin(i/8*2*np.pi)
+        normals[2,i] = 1.0
+    tangentials = np.zeros((3,n,2))
+    tangentials[0,:,0] = 1
+    tangentials[1,:,1] = 1
+    modes = contact_modes.enumerate_contact_separating_3d_exponential(points, normals)
+    sliding_modes = contact_modes.enumerate_all_modes_3d_exponential(points, normals, tangentials, 2)
+    print('contact mode: ')
+    print(modes)
+    print(len(modes))
+    
+    # Create box-against-wall contact manifold.
+    points = np.zeros((3, 8))
+    normals = np.zeros((3, 8))
+    # box on x-y plane
+    points[:, 0] = np.array([1, 1, 0])
+    points[:, 1] = np.array([-1, 1, 0])
+    points[:, 2] = np.array([-1, -1, 0])
+    points[:, 3] = np.array([1, -1, 0])
+    # box against x-z wall
+    points[:, 4] = np.array([1, 1, 2])
+    points[:, 5] = np.array([-1, 1, 2])
+    points[:, 6] = np.array([-1, 1, 0])
+    points[:, 7] = np.array([1, 1, 0])
+
+    normals[2, 0:4] = 1.0
+    normals[1, 4:8] = -1.0
+
+    tangentials = np.zeros((3, 8, 2))
+    tangentials[0, 0:4, 0] = 1
+    tangentials[1, 0:4, 1] = 1
+    tangentials[0, 4:8, 0] = 1
+    tangentials[2, 4:8, 1] = -1
+
+    t_start = time()
+    modes = contact_modes.enumerate_all_modes_3d_exponential(points, normals,tangentials,4)
+    print('time', time() - t_start)
+    print(modes)
+    print(len(modes))
+    # print(modes.shape)
+    '''
+
+test_enum_contact_all_3d()
+#print(in_hull(np.array([[3,0,0]]),np.array([[1,1,0],[1,-1,0],[-1,1,0],[-1,-1,0]])))
