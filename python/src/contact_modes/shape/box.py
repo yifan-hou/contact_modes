6b07ea76d35593550a7a1351e5b5dfd4620d678d
Eric Huang <erich1@andrew.cmu.edu>
2020-01-24 12:30:59 -0500

    Faster supmap for box

--- python/src/contact_modes/shape/box.py
+++ python/src/contact_modes/shape/box.py
@@ -13,28 +13,39 @@ class Box(HalfedgeMesh):
         self.x = x
         self.y = y
         self.z = z
-        x = x - 2 * self.margin()
-        y = y - 2 * self.margin()
-        z = z - 2 * self.margin()
+        # x = x - 2 * self.margin()
+        # y = y - 2 * self.margin()
+        # z = z - 2 * self.margin()
         V = np.zeros((3,8))
         V[0,:] = 0.5*np.array([x, x, x, x, -x, -x, -x, -x])
         V[1,:] = 0.5*np.array([y, -y, y, -y, y, -y, y, -y])
         V[2,:] = 0.5*np.array([-z, -z, z, z, -z, -z, z, z])
         self.build_convex(V)
 
-    # def supmap(self, v):
-    #     tf = self.get_tf_world()
-    #     x0 = self.x - 2 * self.margin()
-    #     y0 = self.y - 2 * self.margin()
-    #     z0 = self.z - 2 * self.margin()
-    #     x_max_dot = -np.Inf
-    #     x_max = np.zeros((3,1))
-    #     for x in [-x0/2, x0/2]:
-    #         for y in [-y0/2, y0/2]:
-    #             for z in [-z0/2, z0/2]:
-    #                 pt = np.array([x, y, z])
-    #                 pt = SE3.transform_point(tf, pt.reshape((3,1)))
-    #                 if x_max_dot < np.dot(v.T, pt).item():
-    #                     x_max_dot = np.dot(v.T, pt).item()
-    #                     x_max = x
-    #     return x_max
+    def supmap(self, v):
+        tf = self.get_tf_world()
+
+        x0 = self.x - 2 * self.margin()
+        y0 = self.y - 2 * self.margin()
+        z0 = self.z - 2 * self.margin()
+
+        # x_max_dot = -np.Inf
+        # x_max = np.zeros((3,1))
+        # for x in [-x0/2, x0/2]:
+        #     for y in [-y0/2, y0/2]:
+        #         for z in [-z0/2, z0/2]:
+        #             pt = np.array([x, y, z])
+        #             pt = SE3.transform_point(tf, pt.reshape((3,1)))
+        #             if x_max_dot < np.dot(v.T, pt).item():
+        #                 x_max_dot = np.dot(v.T, pt).item()
+        #                 x_max = pt
+
+        v = SO3.transform_point_by_inverse(tf.R, v)
+        x = np.multiply(np.sign(v), np.array([x0/2, y0/2, z0/2]).reshape((3,1)))
+        x = SE3.transform_point(tf, x)
+
+        # v = SO3.transform_point(tf.R, v)
+        # x_dot = (v.T @ x).item()
+        # assert(np.linalg.norm(x_dot - x_max_dot) < 1e-9)
+
+        return x
