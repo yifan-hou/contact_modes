7c0293482b3da363e3a7509452f2713dd5505b47
Eric Huang <erich1@andrew.cmu.edu>
2020-02-01 14:04:33 -0500

    Add chain

---
+++ python/src/contact_modes/shape/chain.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sympy as sym
+
+
+class Chain(Shape):
+    def __init__(self, n):
+        pass
+    
+    def generate(self):
+        # 
+        sym.sym
+
+    def set_dofs(self, q):
+        pass
+
+    def get_dofs(self, q):
+        pass
+
+class ChainLink(Shape):
+    def __init__(self):
+        pass
+
+    
