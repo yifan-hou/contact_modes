6fcf6f9ba175468a2225ea7503c2750b44b84a0e
Eric Huang <erich1@andrew.cmu.edu>
2020-03-28 13:28:49 -0400

    Add gperf to benchmarks

--- cpp/bench/bench_arrangements.cpp
+++ cpp/bench/bench_arrangements.cpp
@@ -7,19 +7,39 @@ int main() {
     // Set random seed.
     srand(0);
 
-    // Benchmark initial arrangements.
-    for (int n = 5; n < 10; n++) {
-        Eigen::MatrixXd A(n,n);
-        Eigen::VectorXd b(n);
+    // // Benchmark initial arrangements.
+    // for (int n = 5; n < 10; n++) {
+    //     Eigen::MatrixXd A(n,n);
+    //     Eigen::VectorXd b(n);
 
+    //     A.setRandom();
+    //     b.setRandom();
+    //     auto start = std::chrono::high_resolution_clock::now();
+    //     IncidenceGraphPtr I = initial_arrangement(A, b, 1e-8);
+    //     auto end = std::chrono::high_resolution_clock::now();
+    // }
+
+    // Benchmark increment arrangements.
+    for (int j = 0; j < 10; j++)
+    {
+        srand(0);
+
+        int n = 12;
+        int d = 11;
+        Eigen::MatrixXd A(d,d);
+        Eigen::VectorXd b(d);
         A.setRandom();
         b.setRandom();
-        auto start = std::chrono::high_resolution_clock::now();
-        IncidenceGraphPtr I = initial_arrangement(A, b, 1e-8);
-        auto end = std::chrono::high_resolution_clock::now();
 
-        // std::cout << n << " " << 
-        // std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / 1e6
-        // << " ms" << std::endl;
+        IncidenceGraphPtr I = initial_arrangement(A, b, 1e-8);
+        // I->update_sign_vectors(1e-8);
+
+        for (int i = d; i < n; i++) {
+            Eigen::VectorXd a(d);
+            Eigen::VectorXd b(1);
+            a.setRandom();
+            b.setRandom();
+            increment_arrangement(a, b[0], I, 1e-8);
+        }
     }
 }
